72facc25124a74f740dca7d7610c02e923db201a
Witaliikaplun <vitalijkaplun@gmail.com>
2020-02-27 20:48:04 +0300

    Initial commit

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,162 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+public class ClientHandler {
+    Socket socket = null;
+    DataInputStream in;
+    DataOutputStream out;
+    Server server;
+    private String nick;
+    private String login;
+
+
+    public ClientHandler(Socket socket, Server server) {
+        try {
+            this.socket = socket;
+            this.server = server;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    socket.setSoTimeout(120000);
+                    //цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/reg ")) {
+                            String[] token = str.split(" ");
+                            boolean b = server
+                                    .getAuthService()
+                                    .registration(token[1],token[2], token[3]);
+                            if(b){
+                                sendMsg("Регистрация прошла успешно");
+                            } else {
+                                sendMsg("Пользователь не может быть зарегистрирован");
+                            }
+                        }
+
+                        if (str.equals("/end")) {
+                            throw new RuntimeException("сами ");
+                        }
+                        if (str.startsWith("/auth ")) {
+                            String[] token = str.split(" ");
+                            if (token.length < 3) {
+                                continue;
+                            }
+                            String newNick = server
+                                    .getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+                            if (newNick != null) {
+                                login = token[1];
+                                if (!server.isLoginAuthorized(login)) {
+                                    sendMsg("/authok " + newNick);
+                                    nick = newNick;
+                                    server.subscribe(this);
+                                    System.out.println("Клиент " + nick + " подключился");
+                                    socket.setSoTimeout(0);
+                                    break;
+                                } else {
+                                    sendMsg("С этим логином уже авторизовались");
+                                }
+                            } else {
+                                sendMsg("Неверный логин / пароль");
+                            }
+                        }
+                    }
+
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                out.writeUTF("/end");
+                                break;
+                            }
+                            if (str.startsWith("/w ")) {
+                                String[] token = str.split(" ", 3);
+                                if (token.length == 3) {
+                                    server.privateMsg(this, token[1], token[2]);
+                                }
+                            }
+                        } else {
+                            server.broadcastMsg(nick, str);
+                        }
+
+
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("сами вызвали исключение.");
+                } catch (SocketTimeoutException e){
+                    try {
+                        System.out.println("Отключение " + nick + " по timeout:" + socket.getSoTimeout()/1000 +" с");
+                        if(nick == null){
+                            try {
+                                out.writeUTF("/end");
+                                out.writeUTF("/end");
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        } else {
+                            try {
+                                out.writeUTF("/end");
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    } catch (SocketException ex) {
+                        ex.printStackTrace();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Клиент отключился");
+                }
+            }).start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
